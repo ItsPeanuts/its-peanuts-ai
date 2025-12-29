7f084442bff00a4df35f1302d897c217ed5719a1
ItsPeanuts <joshua.brouwn@gmail.com>
2025-12-29 16:01:56 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,20 +1,25 @@
 from fastapi import FastAPI
-from backend.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
+
 from backend.routers import auth as auth_router
-from backend.routers import dev as dev_router
 
 app = FastAPI(title="It's Peanuts AI")
 
-# create tables
-Base.metadata.create_all(bind=engine)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router.router)
-app.include_router(dev_router.router)
 
 
 @app.get("/healthz")
 def healthz():
     return {"status": "ok"}
+
 
 
 
