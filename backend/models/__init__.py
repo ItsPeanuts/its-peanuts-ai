d719ba835213fbb5c8013943a704be5e7042f1c1
ItsPeanuts <joshua.brouwn@gmail.com>
2026-01-03 16:03:32 +0100

    Update __init__.py

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,9 +1,10 @@
+# backend/models/__init__.py
+
 from backend.database import Base
+
 from .candidate import Candidate
 from .cv import CandidateCV
 from .vacancy import Vacancy
 from .application import Application
 
 __all__ = ["Base", "Candidate", "CandidateCV", "Vacancy", "Application"]
-
-
