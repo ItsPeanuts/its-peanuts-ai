2265afba50c8aacfa4d104692267dcc9f2005e20
ItsPeanuts <joshua.brouwn@gmail.com>
2026-01-03 15:34:14 +0100

    Update __init__.py

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -1,15 +1,14 @@
 from backend.database import Base
 
-# Import all models so SQLAlchemy can discover them
 from .candidate import Candidate
-from .vacancy import Vacancy
 from .candidate_cv import CandidateCV
+from .vacancy import Vacancy
 
 __all__ = [
     "Base",
     "Candidate",
-    "Vacancy",
     "CandidateCV",
+    "Vacancy",
 ]
 
 
